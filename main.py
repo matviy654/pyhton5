df79db8c28ee817aef0a5cc370bfee08a8846bcb
matviy654 <matviy22.2008@gmail.com>
2024-07-01 20:30:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# import random
+
+# list1 = [random.randint(1, 20) for _ in range(10)]
+# list2 = [random.randint(1, 20) for _ in range(10)]
+
+# print("Список 1:", list1)
+# print("Список 2:", list2)
+
+# merged_list = list1 + list2
+# print("Третій список (елементи обох списків):", merged_list)
+
+# unique_list = list(set(merged_list))
+# print("Третій список без повторів:", unique_list)
+
+
+# intersection_list = list(set(list1) & set(list2))
+# print("Третій список зі спільними елементами:", intersection_list)
+
+
+# union_list = list(set(list1) | set(list2))
+# print("Третій список з унікальними елементами кожного зі списків:", union_list)
+
+
+# min_max_list = [min(list1), max(list1), min(list2), max(list2)]
+# print("Третій список з мінімальними та максимальними значеннями кожного зі списків:", min_max_list)
